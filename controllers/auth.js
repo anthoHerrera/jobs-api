de40653eb75a60866f630aafa52ddec919fcccf4
Anthony Herrera <ankehecr100@gmail.com>
2021-10-07 09:44:25 -0500

    added register funcionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,9 @@ const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, UnauthenticatedError } = require("../errors");
 
 const register = async (req, res) => {
-    res.json(req.body);
+    const user = User.create({ ...req.body });
+    const token = user.createJWT();
+    res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
