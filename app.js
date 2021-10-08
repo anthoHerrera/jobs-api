d630a3e7ae7635424bcb6dfae44f558c075341de
Anthony Herrera <ankehecr100@gmail.com>
2021-10-08 12:20:59 -0500

    added security middleware

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 require("dotenv").config();
 require("express-async-errors");
 
+// extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const express = require("express");
 const app = express();
 
@@ -16,7 +22,17 @@ const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
 // middleware
+app.use("trust proxy", 1);
+app.use(
+    rateLimiter({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100, // Limit each IP to 100 requests per windows
+    })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // extra packages
 
